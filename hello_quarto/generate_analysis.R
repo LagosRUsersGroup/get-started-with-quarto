c55dbf401484484faf5e0ceee388bb33dbd8753d
Ayomide Akinwande <akinwandeayomide24@gmail.com>
2024-05-31 02:26:10 +0100

    Remove poor code

--- hello_quarto/generate_analysis.R
+++ hello_quarto/generate_analysis.R
@@ -20,24 +20,3 @@ generate_analysis_report("Asia")
 generate_analysis_report("Africa")
 generate_analysis_report("Americas")
 generate_analysis_report("Oceania")
-
-
-
-# continent_analysis <- gapminder |>
-#   distinct(continent) |>
-#   mutate(
-#     continent = as.character(continent),
-#     output_format = "html",
-#     output_file = paste0(continent, "_analysis.html"),
-#     execute_params = purrr::map(
-#       continent, \(continent) list(continent_name = continent)
-#     )
-#   ) |>
-#   select(-continent)
-# 
-# purrr::pwalk(
-#   continent_analysis,
-#   quarto::quarto_render,
-#   input = "hello_quarto.qmd",
-#   .progress = TRUE
-# )
